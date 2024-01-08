8e21574058de7cafb35a17bbd5d267374571fa66
WoutKeulen <97833571+WoutKeulen@users.noreply.github.com>
2024-01-08 08:42:24 +0100

    Added python file

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+import numpy as np
+
+# some method to obtain coordinates for roads
+
+
+# assumed structure for below path finding algorithm:
+# 1 start point of road
+# 2 intersection point of roads
+# 3 end point of roads
