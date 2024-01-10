73a7ca426fb1f1b0d95eae64a7bdd941fc80f3bb
WoutKeulen <woutkeulen@gmail.com>
2024-01-10 16:06:17 +0100

    expanded coordinate calculation

--- main.py
+++ main.py
@@ -38,8 +38,8 @@ def haversine(lon1, lat1, lon2, lat2):
     return c * r
 
 
-tester = haversine(bp_lon, bp_lat, 5, 50)
-print(tester)
+# tester = haversine(bp_lon, bp_lat, 5, 50)
+# print(tester)
 
 # inverse haversine function
 # def havers_inv()
@@ -68,24 +68,71 @@ def get_point_at_distance(lat1, lon1, d, bearing, R=6378.137):
 
 lat = 50.85217062538557
 lon = 5.6906075839614285
-distance = 5
-bearing1 = 90
-bearing2 = 180
-bearing3 = 270
-bearing4 = 0
-lat2_90, lon2_90 = get_point_at_distance(lat, lon, distance, bearing1)
-lat2_180, lon2_180 = get_point_at_distance(lat, lon, distance, bearing2)
-lat2_270, lon2_270 = get_point_at_distance(lat, lon, distance, bearing3)
-lat2_0, lon2_0 = get_point_at_distance(lat, lon, distance, bearing4)
+# distance = 5
+# bearing1 = 90
+# bearing2 = 180
+# bearing3 = 270
+# bearing4 = 0
+# lat2_90, lon2_90 = get_point_at_distance(lat, lon, distance, bearing1)
+# lat2_180, lon2_180 = get_point_at_distance(lat, lon, distance, bearing2)
+# lat2_270, lon2_270 = get_point_at_distance(lat, lon, distance, bearing3)
+# lat2_0, lon2_0 = get_point_at_distance(lat, lon, distance, bearing4)
 
-print(lat2_90, lon2_90)
-print(lat2_180, lon2_180)
-print(lat2_270, lon2_270)
-print(lat2_0, lon2_0)
+# print(lat2_90, lon2_90)
+# print(lat2_180, lon2_180)
+# print(lat2_270, lon2_270)
+# print(lat2_0, lon2_0)
+
+# triangle on circle:
+input_dist = 10.0
+error_factor = 1.47
+adjusted_input_dist = input_dist / error_factor
+# drawing circle with points, 90, 45, 45
+# radius = input_dist / (2 + np.sqrt(2))
+# drawing circle with points, 0, 45, 90, 0
+# radius = input_dist / (2 + 4 * np.tan(radians(22.5)))
+# radius with adjusted input distance
+radius = adjusted_input_dist / (2 + 4 * np.tan(radians(22.5)))
+# angles for first test cirkel
+angle_0 = 0
+angle_45 = 45
+angle_90 = 90
+# triangle one, test cirkel
+lat2_0, lon2_0 = get_point_at_distance(lat, lon, radius, angle_0)
+lat2_45, lon2_45 = get_point_at_distance(lat, lon, radius, angle_45)
+lat2_90, lon2_90 = get_point_at_distance(lat, lon, radius, angle_90)
+
+print(lat, ',', lon)
+print(lat2_0, ",", lon2_0)
+print(lat2_45, ",", lon2_45)
+print(lat2_90, ",", lon2_90)
+
+total_distance = 0.0
+total_distance += haversine(lon, lat, lon2_0, lat2_0)
+total_distance += haversine(lon2_0, lat2_0, lon2_45, lat2_45)
+total_distance += haversine(lon2_45, lat2_45, lon2_90, lat2_90)
+total_distance += haversine(lon, lat, lon2_90, lat2_90)
+print(total_distance)
+
+wyp1a, wyp1b = str(lat2_0), str(lon2_0)
+wyp1 = wyp1a + "," + wyp1b
+
+wyp2a, wyp2b = str(lat2_45), str(lon2_45)
+wyp2 = wyp2a + "," + wyp2b
+
+wyp3a, wyp3b = str(lat2_90), str(lon2_90)
+wyp3 = wyp3a + "," + wyp3b
+
+print(wyp1, wyp2, wyp3)
 
 
+'''
 
-# google api
+# translate coordinate values into strings:
+wyp1 = str(lat2_0)
+
+
+# read google api from txt key
 api_file = open("api_key.txt", "r")
 api_key = api_file.readline()
 api_file.close()
@@ -93,5 +140,16 @@ api_file.close()
 # starting point input
 st_point_lat, st_point_lon = 50.85217062538557, 5.6906075839614285
 
+dest_lat_lon = "50.85217062538557,5.6906075839614285"
+
 # end point input
 # end_point_lat, end_point_lon = 50.85217062538557, 5.6906075839614285
+
+# base url https://maps.googleapis.com/maps/api/directions/outputFormat?parameters
+# for now using coordinates, place ID is more efficient
+# perhaps later implement a way to get place ID from given coordinates
+url = "https://maps.googleapis.com/maps/api/directions/json?"
+
+# get a response
+r = requests.get(url +"destination=" +dest_lat_lon + "&origin=" + dest_lat_lon + "&mode=walking" + "&units=metric" + )
+'''
